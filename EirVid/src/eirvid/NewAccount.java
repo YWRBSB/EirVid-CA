404c0ee93204b734758518eecd8c8e37e084e417
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-12-14 16:14:03 +0000

    Update NewAccount.java

--- EirVid/src/eirvid/NewAccount.java
+++ EirVid/src/eirvid/NewAccount.java
@@ -2,47 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-//package eirvid;
-//
-///**
-// *
-// * @author Yuri
-// */
-//
-//    
-//    public class NewAccount implements NewAccountInterface {
-//    public void StoreTrades(List<AccountInfo> NewAccount) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
-//        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-//        String DB_URL = "jdbc:mysql://localhost/Account";
-//        String USER = "root";
-//        String PASS = "root";    
-//        
-//        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-//           Statement stmt = conn.createStatement();
-//        ) {		
-//            String sql;
-////            sql = "CREATE TABLE IF NOT EXISTS Account " +
-////                    "(id INTEGER not NULL AUTO_INCREMENT, " +
-////                    " Email VARCHAR(255), " + 
-////                    " Password VARCHAR(255), " + 
-////                    " PRIMARY KEY ( id ));"; 
-////            stmt.executeUpdate(sql);
-////            System.out.println("Created table in given database...");  
-//            
-//            
-//            for (AccountInfo Account : NewAccount) {
-//                    sql = String.format("INSERT INTO Account (Email, Password) VALUES ('%s', '%s');",
-//                    
-//                    NewAccount.Email, NewAccount.Password);
-//                    stmt.executeUpdate(sql);
-//            }
-//            System.out.println("Sucessful");
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();        
-//        }          
-//    }
-//}
+package eirvid;
+
+import java.sql.*;
+import java.util.Scanner;
+/**
+ *
+ * @author Yuri
+ */
+
+import javax.print.DocFlavor.STRING;
+
+    
+    public class NewAccount {
+
+        static final String DB_URL = "jdbc:mysql://localhost/Account";
+        static final String USER = "root";
+        static final String PASS = "root";  
+        
+        //open a connection 
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)
+        Statement stmt = conn.createStatement();){
+
+            String sql = "CREATE DATABASE Account";
+            stmt.executeUpdate(sql);
+            System.out.println("Database created successfully...");
+                  
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+}
 
     
 
